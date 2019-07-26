a47b661e4365bf7977f738c798a92e89d7b54d8b
Jun Hyun Bang <flintintoe@gmail.com>
2019-07-26 13:23:06 +0900

    Add files via upload

---
+++ day 20/HelloWorld.java
@@ -0,0 +1,89 @@
+package day20.helloworld;
+
+// Hello world 출력, 변수 출력
+
+public class HelloWorld {
+	public static void main(String[] args) {
+//		System.out.println("Hello world!");
+//
+//		int x = 3;
+//		char ch = 'c';
+//
+//		System.out.println("The number is " + x);
+//		System.out.println("The char is " + ch);
+//
+//		System.out.println(3);
+//		System.out.println('c');
+//
+//		double y = 1.5D;
+//
+//		System.out.println(y);
+//		
+//		int octal = 023; // 8 진수
+//		int hex = 0xC; // 16 진수
+//		
+//		int a = 10;
+//		int b = 15;
+//		
+//		System.out.println(a + b);
+//		System.out.println(a - b);
+//		System.out.println(a * b);
+//		System.out.println(a / b);
+//		System.out.println(a % b);
+//		System.out.println(a ^ b);
+		
+//		int i = 0;
+//		int j = 10;
+//		
+//		i++;
+//		j++;
+//		
+//		System.out.println(i + ", " + j);
+//		System.out.println(i++);
+//		System.out.println(++i);
+//		
+//		boolean isCheese = false;
+//		boolean isSame = i == j; // 부욜 연산
+//		
+//		if(!isCheese) {
+//			System.out.println("It's not cheese!");
+//		}
+		
+//		int base = 100;
+//		int height = 195;
+//		
+//		boolean isTall = height > base;
+//		
+//		if(isTall) {
+//			System.out.println("키가  큽니다");
+//		}
+		
+//		int i = 3;
+//		boolean isOdd = 3 % 2 == 1;
+//		
+//		if(isOdd) {
+//			System.out.println("홀수 ");
+//		}
+		
+//		char ch1 = 'a';
+//		char ch2 = 97;
+//		char ch3 = '\u0061';
+//		
+//		System.out.println(ch1 + " " + ch2 + " " + ch3);
+		
+		// byte short long int char double boolean <-- 원식 (primitive) 자료형)
+		// String <-- 문자열 클래스
+		
+		String str = "Hello world";
+		String str2 = "java";
+		
+		System.out.println(str);
+		
+		if(str.equals(str2)) {
+			System.out.println("Both strings are equal");
+		} else {
+			System.out.println("Both strings are not equal");
+		}
+	}
+
+}
